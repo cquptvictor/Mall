f54cf6f3c5a98223731ce9bffff2317ac239119d
victor <943382615@qq.com>
2019-08-09 21:55:10 +0800

    添加了登录

--- src/main/java/com/cqupt/zyx/Controller/Backstage/LoginController.java
+++ src/main/java/com/cqupt/zyx/Controller/Backstage/LoginController.java
@@ -1,23 +1,26 @@
 package com.cqupt.zyx.Controller.Backstage;
 
-import com.cqupt.zyx.Service.ServiceForBackstage.SysUserService;
+import com.cqupt.zyx.Service.ServiceForBackstage.impl.SysUserServiceImpl;
 import com.cqupt.zyx.domain.beanForBackstage.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * 后台的登录控制*/
 @Controller
 public class LoginController {
     @Autowired
-    SysUserService sysUserService;
+    SysUserServiceImpl sysUserService;
 
     @RequestMapping(value = "/login")
     @ResponseBody
-    public String login(SysUser sysUser){
+    public String login(SysUser sysUser, HttpSession httpSession){
         if(sysUserService.identify(sysUser)){
+            httpSession.setAttribute("isLogin",true);
             return "{'static':1}";
         }else
             return "{'static':0}";
