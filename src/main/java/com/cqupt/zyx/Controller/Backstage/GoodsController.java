11c21df8b03308c4c58f03876d96cba46ddc0fbf
victor <943382615@qq.com>
2019-08-14 21:37:48 +0800

    改善了一下文件上传

--- src/main/java/com/cqupt/zyx/Controller/Backstage/GoodsController.java
+++ src/main/java/com/cqupt/zyx/Controller/Backstage/GoodsController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
--- src/main/java/com/cqupt/zyx/Utils/FileUploadUtil.java
+++ src/main/java/com/cqupt/zyx/Utils/FileUploadUtil.java
@@ -4,28 +4,30 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 public class FileUploadUtil {
-    private static final String base = "C:\\Users\\victor\\Desktop\\SSM学习\\d14c3o\\Mall\\src\\main\\webapp\\WEB-INF\\uploadFile";
+    private static final String base = "C:\\Users\\victor\\Desktop\\SSM学习\\d14c3o\\Mall\\src\\main\\webapp\\upload\\";
     public static String save(MultipartFile multipartFile,String fileName){
             int dir1 = fileName.hashCode()&0xf;
             int dir2 = fileName.hashCode()&0xf0;
-            String path  = base+File.separator+dir1+File.separator+dir2+File.separator+fileName+".jpg";
-            File file = new File(path);
-            System.out.println(file.exists());
-            if(!file.exists()){
-                file.mkdirs();
-            }
+
+            String path  = base + dir1+File.separator+dir2+File.separator;
+            File file = new File(path,new Date().getTime()+".jpg");
+            if(!file.getParentFile().exists())
+                file.getParentFile().mkdirs();
         try {
             multipartFile.transferTo(file);
         } catch (IOException e) {
+            e.printStackTrace();
             return null;
         }
-        return path;
+        return file.getPath().substring(58);
     }
     public static void main(String[] args){
-        File file = new File("src/main/webapp/WEB-INF/uploadFile/12/64");
-        if(!file.exists())
-            file.mkdirs();
+    System.out.println("C:\\Users\\victor\\Desktop\\SSM学习\\d14c3o\\Mall\\src\\main\\webapp\\upload\\".lastIndexOf("upload"));
+        System.out.println("C:\\Users\\victor\\Desktop\\SSM学习\\d14c3o\\Mall\\src\\main\\webapp\\upload\\".substring(58));
+
     }
+
 }
